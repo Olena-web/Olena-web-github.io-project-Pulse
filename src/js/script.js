521016c5b6bb5c3ccdfbb4eda2b19cf7b83c2680
Olena Honcharuk <gasmin07@gmail.com>
2020-12-24 22:38:07 +0200

    three containers, carousel slick

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        //adaptiveHeight: true,
+        //autoplay: true,
+        //autoplaySpeed: 2000,
+        //fade: true,
+        //cssEase: 'linear'
+        prevArrow:'<type="button" class="slick-prev"><img src="icons/chevron_left.jpg"></button>',
+        nextArrow:'<type="button" class="slick-next"><img src="icons/chevron_right.jpg"></button>',
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                dots: true,
+                arrows: false,
+                }
+            } 
+        ],
+      });
+  });
