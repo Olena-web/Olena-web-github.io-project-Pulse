8a62c027aa3eb084ac767e2537aaaee9db68240f
Olena Honcharuk <gasmin07@gmail.com>
2021-01-13 18:32:16 +0200

    tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,23 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
+//$(document).ready(function(){
+    //$('.carousel__inner').slick({
+        //speed: 1200,
         //adaptiveHeight: true,
         //autoplay: true,
         //autoplaySpeed: 2000,
         //fade: true,
         //cssEase: 'linear'
-        prevArrow:'<type="button" class="slick-prev"><img src="icons/chevron_left.jpg"></button>',
-        nextArrow:'<type="button" class="slick-next"><img src="icons/chevron_right.jpg"></button>',
-        responsive:[
-            {
-                breakpoint: 992,
-                settings: {
-                dots: true,
-                arrows: false,
-                }
-            } 
-        ],
-      });
-  });
+        //prevArrow:'<type="button" class="slick-prev"><img src="icons/chevron_left.jpg"></button>',
+        //nextArrow:'<type="button" class="slick-next"><img src="icons/chevron_right.jpg"></button>',
+        //responsive:[
+           // {
+                //breakpoint: 992,
+               // settings: {
+               // dots: true,
+                //arrows: false,
+               // }
+           // } 
+       // ],
+      //});
+  //})];
+    
+  
