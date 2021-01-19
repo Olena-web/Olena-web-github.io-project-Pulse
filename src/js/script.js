122c6a7d53fc47249bc0d3ece945cb78c3da0ba5
Olena Honcharuk <gasmin07@gmail.com>
2021-01-19 22:49:17 +0200

    added jquery-script for tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-//$(document).ready(function(){
+$(document).ready(function(){
     //$('.carousel__inner').slick({
         //speed: 1200,
         //adaptiveHeight: true,
@@ -19,5 +19,9 @@
        // ],
       //});
   //})];
-    
-  
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+});
